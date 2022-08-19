df9997dc0d55d5fd72c3761d14a16d06eb697269
Mohamed Mohy <mohamed.mohy.info@gmail.com>
2022-08-19 12:51:46 +0200

    Create Login Screen

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,6 +2,8 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+       setSupportActionBar(toolbar)
         Timber.plant(Timber.DebugTree())
     }
 }
